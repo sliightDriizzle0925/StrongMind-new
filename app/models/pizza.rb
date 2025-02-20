3786fc2a4f859c4d6068293fd10989ca2df4852f
sliightDriizzle <capybara0711@gmail.com>
2025-02-20 11:43:58 -0700

    Validation for existing pizzas

--- app/models/pizza.rb
+++ app/models/pizza.rb
@@ -1,5 +1,6 @@
 class Pizza < ApplicationRecord
     validates :name, presence: true
+    validate :unique_toppings_combination
 
     def toppings_list
         toppings.present? ? toppings.split(', ') : []
@@ -7,5 +8,13 @@ class Pizza < ApplicationRecord
 
     def toppings_list=(values)
         self.toppings = values.reject(&:blank?).join(', ')
+    end
+
+    private
+    def unique_toppings_combination
+        existing_pizza = Pizza.find_by(toppings: toppings)
+        if Pizza.where(toppings: toppings).exists?
+            errors.add(toppings, "> this combination of toppings already exists on another pizza: #{existing_pizza.name}!")
+        end
     end
 end
